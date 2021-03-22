d332103fc39c4928809fe2cfae789c3ed0c593ea
laxmi <laxmimenon18@gmail.com>
2021-03-22 12:21:41 +0530

    error one

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,12 +1,14 @@
-const fn = (posts = [], action) => {
+import { FETCH_ALL, CREATE } from '../constants/actionTypes.js'
+
+const variable = (posts = [], action) => {
     switch (action.type) {
-        case 'FETCH_ALL':
-            return action.payload;
-            case 'CREATE':
-                return [...posts, action.payload];
-        default:
-                return posts;
+      case FETCH_ALL:
+        return action.payload;
+      case CREATE:
+        return [...posts, action.payload];
+      default:
+        return posts;
     }
 };
 
-export default fn;
+export default variable;
